0597a5b9518a2f440ef85990ee42eec37492ead3
Nadir Metrouh <mnadir20@gmail.com>
2023-11-21 15:15:10 +0100

    specify lambda function

--- main.tf
+++ main.tf
@@ -10,3 +10,22 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+//  Define Lymda fonction
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = data.archive_file.lambda.output_path
+  function_name = "my-first-tf-lambda-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs18.x"
+}
